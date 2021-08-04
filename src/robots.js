694834d781fa610870f9567544d6c960e3ad15c5
mdtitong <markdave.titong@gmail.com>
2021-08-04 16:47:56 -0400

    first commit

---
+++ src/robots.js
@@ -0,0 +1,81 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Marko Dabid',
+        username: 'Marko',
+        email: 'marko.dabid@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Erica Lav',
+        username: 'Elav',
+        email: 'erica.lav@yahoo.com'
+    },
+    {
+        id: 3,
+        name: 'Kael Damian',
+        username: 'Kaeli',
+        email: 'kael.damian@aol.com'
+    },
+    {
+        id: 4,
+        name: 'Pahong Sila',
+        username: 'Pahong',
+        email: 'pahong.sila@ambot.com'
+    },
+    {
+        id: 5,
+        name: 'Buang Ka',
+        username: 'Buang',
+        email: 'buang.ka@bahala.com'
+    },
+    {
+        id: 6,
+        name: 'Mari Gallagher',
+        username: 'Mari',
+        email: 'mari.gallagher@ymail.com'
+    },
+    {
+        id: 7,
+        name: 'Tom Mosley',
+        username: 'Tom',
+        email: 'tom.mosley@yahoo.com'
+    },
+    {
+        id: 8,
+        name: 'Najma Mercer',
+        username: 'Najma',
+        email: 'najma.mercer@bahala.com'
+    },
+    {
+        id: 9,
+        name: 'Leonidas Schneider',
+        username: 'Leonidas',
+        email: 'leonidas.schneider@email.com'
+    },
+    {
+        id: 10,
+        name: 'Santino Kavanagh',
+        username: 'Santino',
+        email: 'santino.kavanagh@email.com'
+    },
+    {
+        id: 11,
+        name: 'Hasula Man',
+        username: 'Hasula',
+        email: 'hasula@email.com'
+    },
+    {
+        id: 12,
+        name: 'Hasula Man',
+        username: 'Hasula',
+        email: 'hasula@email.com'
+    },
+    {
+        id: 13,
+        name: 'Santino Kavanagh',
+        username: 'Santino',
+        email: 'santino.kavanagh@email.com'
+    }
+
+];
